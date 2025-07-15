729189ea2e3b5e48e2509b1e80299c9a0dfb6f6b
Justin <justcant1gmail.com>
2025-07-15 15:14:27 -0400

    animated menus and updated images

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+function toggleMenu() {
+  const menu = document.getElementById('hamburger-menu');
+  const links = document.getElementById('page-heading-links');
+
+ if (links.style.display === 'block') {
+    links.style.display = 'none';
+    menu.classList.remove('open');
   } else {
-    x.style.display = "block";
+    links.style.display = 'block';
+    menu.classList.add('open');
   }
 }
+
+document.addEventListener('click', (e) => {
+  const targetId = e.target.id;
+  const idsToIgnore = ['hamburger-menu', 'about-nav-item', 'projects-nav-item', 'contact-nav-item'];
+  const ignoreId = idsToIgnore.some(id => id === targetId);
+
+  if (ignoreId) {
+    return;
+  }
+
+  const links = document.getElementById('page-heading-links');
+  const menu = document.getElementById('hamburger-menu');
+
+  menu.classList.remove('open');
+  links.style.display = 'none';
+});
