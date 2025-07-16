cb0c244ffcb189f9a1ce28993fe26910666a5e93
Justin <justcant1gmail.com>
2025-07-16 10:30:10 -0400

    work on hero section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-  const menu = document.getElementById('hamburger-menu');
+  const menu = document.getElementById('page-heading-menu-btn');
   const links = document.getElementById('page-heading-links');
 
  if (links.style.display === 'block') {
@@ -14,14 +14,14 @@ function toggleMenu() {
 document.addEventListener('click', (e) => {
   const targetId = e.target.id;
   const idsToIgnore = [
-    'hamburger-menu', 
+    'page-heading-menu-btn', 
     'about-nav-item', 
     'projects-nav-item', 
     'contact-nav-item',
-    'hamburger-menu-span-1',
-    'hamburger-menu-span-2',
-    'hamburger-menu-span-3',
-    'hamburger-menu-span-4'
+    'page-heading-menu-btn-span-1',
+    'page-heading-menu-btn-span-2',
+    'page-heading-menu-btn-span-3',
+    'page-heading-menu-btn-span-4'
   ];
   const ignoreId = idsToIgnore.some(id => id === targetId);
 
@@ -30,7 +30,7 @@ document.addEventListener('click', (e) => {
   }
 
   const links = document.getElementById('page-heading-links');
-  const menu = document.getElementById('hamburger-menu');
+  const menu = document.getElementById('page-heading-menu-btn');
 
   menu.classList.remove('open');
   links.style.display = 'none';
